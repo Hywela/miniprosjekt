e69401b77c7ccab2ae62b92332a16ec35d00a2fb
Stormert <kevin.mikk@gmail.com>
2013-10-17 12:24:42 +0200

    fuck you git

---
+++ miniprosjekt/src/miniprosjekt/GUI/ButtonLayout.java
@@ -0,0 +1,262 @@
+package miniprosjekt.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Vector;
+
+import javax.print.attribute.standard.OutputDeviceAssigned;
+import javax.swing.DefaultCellEditor;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
+
+import miniprosjekt.model.MyModel;
+import miniprosjekt.variables.BaseMedia;
+
+@SuppressWarnings("serial")
+public class ButtonLayout extends JFrame {
+	private MyModel dataModel = new MyModel ();
+	private JTable table = new JTable (dataModel);
+	private JToolBar toolbar = new JToolBar ();
+	private String layoutType[] = {"JLabel", "JTextField", "JTextArea", "JButton" };
+	private JComboBox<String> animalTypeEditor = new JComboBox<String> (layoutType);
+	private String alignments[] = {"WEST", "NORTHWEST", "SOUTHWEST", "EAST", "NORTHEAST", "SOUTHEAST", "NORTH", "SOUTH", "CENTER" };
+	private JComboBox<String> alignmentEditor = new JComboBox<String> (alignments);
+	private String size[] = {"NONE", "BOTH", "HORIZONTAL", "VERTICAL" };
+	private JComboBox<String> sizeEditor = new JComboBox<String> (size);
+	
+    
+	public ButtonLayout () {
+		super ("Velkommen Media Planleggeren");
+		dataModel.setTableFrame(this);
+		JTextField mm = new JTextField();
+		
+		fileMenu();
+		
+		add (new JScrollPane(table), BorderLayout.CENTER);
+		table.getColumnModel().getColumn(0).setCellEditor(new DefaultCellEditor(animalTypeEditor));
+		table.getColumnModel().getColumn(7).setCellEditor(new DefaultCellEditor(sizeEditor));
+		table.getColumnModel().getColumn(8).setCellEditor(new DefaultCellEditor(alignmentEditor));
+		
+		
+		JButton load = new JButton (new ImageIcon(getClass().getResource("/images/file.png")));
+		load.setToolTipText("Last inn en dyrehage");
+		load.addActionListener(new Load());
+		JButton save = new JButton (new ImageIcon(getClass().getResource("/images/filesave.png")));
+		save.setToolTipText("Lage en dyrehage");
+		save.addActionListener(new Save());
+		JButton saveReport = new JButton (new ImageIcon(getClass().getResource("/images/fileexport.png")));
+		saveReport.setToolTipText("Skriv ut en Show liste");
+		saveReport.addActionListener(new SaveReport());
+		JButton addAnimal = new JButton (new ImageIcon(getClass().getResource("/images/insert_table_row.png")));
+		addAnimal.setToolTipText("Legg til en Serie/Film");
+		addAnimal.addActionListener(new ActionListener (){
+			public void actionPerformed (ActionEvent ae) {
+				dataModel.addMedia();
+			}
+		});
+		toolbar.add (load);
+		toolbar.add (save);
+		toolbar.addSeparator ();
+		toolbar.add (saveReport);
+		toolbar.addSeparator ();
+		toolbar.add (addAnimal);
+		add (toolbar, BorderLayout.NORTH);
+		pack ();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible (true);
+	}
+
+	class Load implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JFileChooser chooser = new JFileChooser(new File("."));
+			chooser.setFileSelectionMode (JFileChooser.FILES_ONLY);
+			if (chooser.showOpenDialog(ButtonLayout.this)==JFileChooser.CANCEL_OPTION)
+				return;
+			File f = chooser.getSelectedFile();
+			try {
+				ObjectInputStream ois = new ObjectInputStream (new FileInputStream(f));
+				dataModel.load (ois);
+				ois.close ();	
+			} catch (IOException ioe) {
+				System.err.println ("Feil på filhåndteringen.");
+			}
+		}
+	}
+	
+	class Save implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JFileChooser chooser = new JFileChooser(new File("."));
+			chooser.setFileSelectionMode (JFileChooser.FILES_ONLY);
+			if (chooser.showSaveDialog(ButtonLayout.this)==JFileChooser.CANCEL_OPTION)
+				return;
+			File f = chooser.getSelectedFile();
+			if (f.exists())
+				if (JOptionPane.showConfirmDialog(ButtonLayout.this, "Filen finnes, overskrive", "Bekreft", JOptionPane.YES_NO_OPTION)!=JOptionPane.YES_OPTION)
+					return;
+			try {
+				ObjectOutputStream oos = new ObjectOutputStream (new FileOutputStream(f));
+				dataModel.save (oos);
+				oos.close ();	
+			} catch (IOException ioe) {
+				System.err.println ("Feil på filhåndteringen.");
+			}
+			
+		}
+	}
+// TODO:: bytt ut example med et generert navn
+	class SaveReport implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			File f = new File("example.java");
+			if (f.exists())
+				if (JOptionPane.showConfirmDialog(ButtonLayout.this, "Filen finnes, overskrive", "Bekreft", JOptionPane.YES_NO_OPTION)!=JOptionPane.YES_OPTION)
+					return;
+			try {
+				BufferedWriter bw = new BufferedWriter (new FileWriter (f));
+				Vector<BaseMedia> layoutType =dataModel.getData(); 
+				bw.write("import java.awt.*;");
+				bw.newLine();
+				bw.write("import javax.swing.*;");
+				
+				bw.newLine();
+				bw.write("public class example extends JPanel {");
+				bw.newLine();
+				for (int i=0; i<layoutType.size(); i++) {
+					bw.write(layoutType.get(i).getDec());
+				
+					bw.newLine();
+				}
+				bw.write("public example() {");
+				bw.newLine();
+			
+				for (int i=0; i<layoutType.size(); i++) {
+				
+				bw.write(layoutType.get(i).toString());
+					bw.newLine();
+				}
+				bw.write("  GridBagLayout layout = new GridBagLayout ();");
+				bw.newLine();
+				bw.write("GridBagConstraints gbc = new GridBagConstraints();");
+				bw.newLine();
+				bw.write("setLayout (layout);");
+				bw.newLine();
+			
+				for (int i=0; i<layoutType.size(); i++) {
+					bw.write(layoutType.get(i).getGridBagPara());
+					bw.newLine();
+					bw.write(layoutType.get(i).getAddToTable());
+				}
+				bw.write("}//end of Constructor");
+				       bw.newLine();
+				      bw.write("public static void main(String[] args) {");
+				      bw.newLine();
+				    bw.write("JFrame frame = new JFrame(\"Example\");");
+			      bw.newLine();
+			      bw.write("frame.setContentPane(new example());");
+			        bw.newLine();
+			      bw.write("frame.pack();");
+			       bw.newLine();
+			        bw.write("frame.setVisible(true);}");
+				       bw.newLine();
+				         bw.write("}//end of file");
+				         bw.close(); 
+			} catch (IOException ioe) {
+				System.err.println ("Feil under skriving av rapporten.");
+			}
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new ButtonLayout();
+
+	}
+	
+	public void fileMenu(){
+		
+		JMenuBar menuBar = new JMenuBar();
+        
+        // Add the menubar to the frame
+        setJMenuBar(menuBar);
+        
+        // Define and add two drop down menu to the menubar
+        JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        
+        // Create and add simple menu item to one of the drop down menu
+        JMenuItem newAction = new JMenuItem("New");
+        JMenuItem openAction = new JMenuItem("Open");
+        JMenuItem saveAction = new JMenuItem("Save");
+        JMenuItem exitAction = new JMenuItem("Exit");
+        JMenuItem genAction = new JMenuItem("Generate Java");
+        JMenuItem newItemAction = new JMenuItem("New Item");
+        
+        //file menu buttons
+        fileMenu.add(newAction);
+        fileMenu.add(openAction);
+        fileMenu.add(saveAction);
+        fileMenu.addSeparator();
+        fileMenu.add(genAction);
+        fileMenu.add(exitAction);
+        
+        //edit menu buttons
+        editMenu.add(newItemAction);
+        
+        
+        //file menu actions
+        newAction.addActionListener(new ActionListener (){
+			public void actionPerformed (ActionEvent ae) {
+				//TODO: add action for new file
+			}
+		});
+        
+        openAction.addActionListener(new Load());
+        
+        saveAction.addActionListener(new Save());
+		
+        genAction.addActionListener(new SaveReport());
+        
+        exitAction.addActionListener(new ActionListener (){
+			public void actionPerformed (ActionEvent ae) {
+				System.exit(0);
+			}
+		});
+        
+        
+        //edit menu button actions
+        newItemAction.addActionListener(new ActionListener (){
+			public void actionPerformed (ActionEvent ae) {
+				dataModel.addMedia();
+			}
+		});
+      
+        
+	}
+}
---
+++ miniprosjekt/src/miniprosjekt/model/MyModel.java
@@ -0,0 +1,227 @@
+package miniprosjekt.model;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.table.AbstractTableModel;
+
+import miniprosjekt.variables.BaseMedia;
+import miniprosjekt.variables.Jbutton;
+import miniprosjekt.variables.Jtextfield;
+import miniprosjekt.variables.Jlabel;
+import miniprosjekt.variables.Jtextarea;
+
+
+@SuppressWarnings("serial")
+public class MyModel extends AbstractTableModel {
+	int i=0; 
+	Vector<BaseMedia> data = new Vector<BaseMedia>();
+	String titles[] = { "Type","VariabelNavn", "Tekst", "Rad", "Kolonne","Rader" ,
+			"Kolonner","Utfylling", "Forankring"};
+	JFrame mainFrame;
+	
+	/*
+	 * Sets the frame that contains the table for this table model
+	 */
+	public void setTableFrame (JFrame f) {
+		mainFrame = f;
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		return titles[column];
+	}
+	
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		switch (columnIndex) {
+			case 0 : return ("").getClass();
+			case 1 : return ("").getClass();
+			case 2 : return ("").getClass();
+			case 3 : return (new Integer(0)).getClass();
+			case 4 : return (new Integer(0)).getClass();
+			case 5 : return (new Integer(0)).getClass();
+			case 6 : return (new Integer(0)).getClass();
+			case 7 : return ("").getClass();
+			case 8 : return ("").getClass();
+		}
+		return ("").getClass();
+	}
+	
+	@Override
+	public int getColumnCount() {
+		return 9;
+	}
+
+	@Override
+	public int getRowCount() {
+		return data.size();
+	}
+
+	@Override
+	public Object getValueAt(int row, int col) {
+		BaseMedia media = data.get(row);
+		if (col>0) {
+			switch (col) {
+				case 1 : return media.getVariabelname();
+				case 2 : return media.getText();
+				case 3 : return media.getRad();
+				case 4 : return media.getKolonne();
+				case 5 : return media.getRader();
+				case 6 : return media.getKolonner();
+				case 7 : return media.getUtfylling();
+				case 8 : return media.getForankring();
+			}
+		} else {
+			 if (media instanceof Jbutton)
+				return "JButton";
+			else if (media instanceof Jtextfield)
+				return "JTextField";
+			else if (media instanceof Jtextarea)
+				return "JTextArea";
+			else if (media instanceof Jlabel)
+				return "JLabel";
+			else if (media instanceof BaseMedia)
+				return "Velg et type Media";
+		}
+		return null;
+	}
+	
+	public void addMedia () {
+		BaseMedia media = new BaseMedia ();
+		data.add (media);
+		fireTableRowsInserted(data.size(), data.size());
+		 media.setVariabelname("ny"+i); 
+		 i++;
+	}
+	
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return true;
+	}
+	
+	@Override
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		BaseMedia media = data.get(rowIndex);
+		if (columnIndex==1)
+			media.setVariabelname((String)aValue);
+		if (columnIndex==2)
+			media.setText((String)aValue);
+		if (columnIndex==3)
+			media.setRad((int)aValue);
+		if (columnIndex==4)
+			media.setKolonne((int)aValue);
+		if (columnIndex==5)
+			media.setRader((int)aValue);
+		if (columnIndex==6)
+			media.setKolonner((int)aValue);
+		if (columnIndex==7){
+			 if (((String)aValue).equals("NONE")) {
+				media.setUtfylling("NONE");
+			 }
+			 if (((String)aValue).equals("BOTH")) {
+					media.setUtfylling("BOTH");
+			 }
+				
+			 if (((String)aValue).equals("HORIZONTAL")) {
+					media.setUtfylling("HORIZONTAL");
+			 }
+				
+			 if (((String)aValue).equals("VERTICAL")) {
+					media.setUtfylling("VERTICAL");
+			 }
+		}
+			 if (columnIndex==8){
+				 if (((String)aValue).equals("WEST")) {
+					media.setForankring("WEST");
+				 }
+				 if (((String)aValue).equals("NORTHWEST")) {
+					 media.setForankring("NORTHWEST");
+				 }
+					
+				 if (((String)aValue).equals("SOUTHWEST")) {
+					 media.setForankring("SOUTHWEST");
+				 }
+					
+				 if (((String)aValue).equals("EAST")) {
+					 media.setForankring("EAST");
+				 }
+				 if (((String)aValue).equals("NORTHEAST")) {
+					 media.setForankring("NORTHEAST");
+				 }
+				 if (((String)aValue).equals("SOUTHEAST")) {
+					 media.setForankring("SOUTHEAST");
+				 }
+				 if (((String)aValue).equals("NORTH")) {
+					 media.setForankring("NORTH");
+				 }
+				 if (((String)aValue).equals("SOUTH")) {
+					 media.setForankring("SOUTH");
+				 }
+				 if (((String)aValue).equals("CENTER")) {
+					 media.setForankring("CENTER");
+				 }
+			
+		}
+		
+		
+		if (columnIndex==0) {
+			BaseMedia newMedia = null;
+			 if (((String)aValue).equals("JButton")) {
+				 newMedia = new Jbutton (media);
+			
+			 } else if (((String)aValue).equals("JTextField")) {
+				newMedia = new Jtextfield (media);
+				//((Jtextfield)newMedia).setBowlPlacement(JOptionPane.showInputDialog (mainFrame, "I hvilket rom står gullfiskbollen?"));
+			 } else if (((String)aValue).equals("JLabel")) {
+					newMedia = new Jlabel(media);
+			 } else if (((String)aValue).equals("JTextArea")) {
+					newMedia = new Jtextarea (media);
+			 }
+			data.set(rowIndex, newMedia);
+		}
+		fireTableCellUpdated(rowIndex, columnIndex);
+	}
+	
+	public void save (ObjectOutputStream oos) {
+		try {
+			for (int i=0; i<data.size(); i++)
+				oos.writeObject(data.get(i));
+		} catch (IOException ioe) {
+			System.err.println ("Feil under skriving til fil");
+		}
+	}
+	
+	public void load (ObjectInputStream ois) {
+		data.clear();
+		try {
+			while (true) {
+				BaseMedia animal = (BaseMedia)ois.readObject();
+				data.add(animal);
+			}
+		} catch (EOFException eofe) {
+			// Slutt på fila
+		} catch (ClassCastException cce) {
+			System.err.println ("OPPPSSSSS, dette var ingen dyrehage");
+		} catch (ClassNotFoundException cnfe) {
+			System.err.println ("Oi, vi har mista ur-dyret.");
+		} catch (IOException ioe) {
+			System.err.println ("Feil under lesing fra fil");
+		} finally {
+			fireTableDataChanged();
+		}
+	}
+	
+	public Vector<BaseMedia> getData () {
+		return data;
+	}
+}
+
+
+
